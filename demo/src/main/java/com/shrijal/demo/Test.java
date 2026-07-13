ff3aa0b73227c67467d010641ee5d7109739e23d
Shrijal Singh <shrijalsingh6393@gmail.com>
2026-07-13 12:42:25 +0530

    IOC

--- demo/src/main/java/com/shrijal/demo/Test.java
+++ demo/src/main/java/com/shrijal/demo/Test.java
@@ -1,4 +1,23 @@
 package com.shrijal.demo;
 
+class P{
+    void m1(){
+
+    }
+
+    void m2(){
+
+    }
+}
+
+class c1 extends P{
+    void m3(){
+
+    }
+}
+
 public class Test {
+    c1 c = new c1();
+//    c1 a = new P();    // we cannot do this because we cannot store the object of parent class
+//    into the child reference
 }
