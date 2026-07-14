32ac2d35145993d395089d47ac0c4a7c3d2ba4d1
Shrijal Singh <shrijalsingh6393@gmail.com>
2026-07-14 15:32:08 +0530

    StudentServer

---
+++ demo/src/main/java/com/shrijal/demo/StudentServer/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.shrijal.demo.StudentServer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+    public Student save(Student student){
+        System.out.println("Student Information saved");
+        return student;
+    }
+}
---
+++ demo/src/main/java/com/shrijal/demo/StudentServer/StudentService.java
@@ -0,0 +1,28 @@
+package com.shrijal.demo.StudentServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student studentValidate(Student student){
+
+        int id = student.getId();
+        String name = student.getName();
+        int age = student.getAge();
+        String department = student.getDepartment();
+
+        if(id < 0 || name == null || age < 0 || department == null){
+            return null;
+        }
+
+        return studentRepository.save(student);
+    }
+}
