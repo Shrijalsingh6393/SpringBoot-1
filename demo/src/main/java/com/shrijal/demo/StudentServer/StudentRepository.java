3db12a35b9bb189de4196127d977f24a95a84eda
Shrijal Singh <shrijalsingh6393@gmail.com>
2026-07-15 11:48:56 +0530

    DataJpa

--- demo/src/main/java/com/shrijal/demo/StudentServer/StudentRepository.java
+++ demo/src/main/java/com/shrijal/demo/StudentServer/StudentRepository.java
@@ -1,10 +1,13 @@
 package com.shrijal.demo.StudentServer;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class StudentRepository {
-    public Student save(Student student){
-        System.out.println("Student Information saved");
-        return student;
-    }
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+//    public Student save(Student student){
+//        System.out.println("Student Information saved");
+//        return student;
+//    }
 }
+
+
+
+// JpaRepository(interface) has methods
