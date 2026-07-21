0c77e5aaad463d57b46d77f5fd780de20355bb0a
Shrijal Singh <shrijalsingh6393@gmail.com>
2026-07-21 10:42:48 +0530

    Scope

--- demo/src/main/java/com/shrijal/demo/StudentServer/DTO/CreateStudentRequestDTO.java
+++ demo/src/main/java/com/shrijal/demo/StudentServer/DTO/CreateStudentRequestDTO.java
@@ -1,17 +1,17 @@
 package com.shrijal.demo.StudentServer.DTO;
 
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class CreateStudentRequestDTO {
-//    @NotBlank(message = "Name is required")
+    @NotBlank(message = "Name is required")
     private String name;
-//    @Positive(message = "Age must be greater than 0")
+    @Positive(message = "Age must be greater than 0")
     private int age;
-//    @NotBlank(message = "Department is required")
+    @NotBlank(message = "Department is required")
     private String department;
 }
